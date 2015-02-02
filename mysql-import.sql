ad1c56aca556737143e346d0dcf6cc979594bc90
Anders Brander <anders@brander.dk>
2015-02-02 10:39:13 +0100

    Added example SQL for importing CSV file to MySQL.

---
+++ mysql-import.sql
@@ -0,0 +1,23 @@
+# Use like:
+# mysql -u user database < mysql-import.sql
+
+DROP TABLE phpusage;
+
+CREATE TABLE phpusage (
+	pid int(11),
+	start timestamp,
+	nice int(11),
+	`real` float,
+	utime float,
+	stime float,
+	cutime float,
+	cstime float,
+	rchar int(11),
+	wchar int(11),
+	syscr int(11),
+	syscw int(11),
+	iowait float,
+	cmdline varchar(100)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+LOAD DATA LOCAL INFILE "/var/log/phpusage.csv" INTO TABLE phpusage CHARACTER SET utf8 FIELDS TERMINATED BY "," ENCLOSED BY "\"" IGNORE 1 LINES;
