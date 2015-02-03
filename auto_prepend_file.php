3ded9b9068256f1a091112e0948b9dbba9444193
Anders brander <anders@brander.dk>
2015-02-03 07:57:45 +0100

    Only do auto statistics in CLI mode. It doesn't make much sense for most web servers.

--- auto_prepend_file.php
+++ auto_prepend_file.php
@@ -1,10 +1,14 @@
 <?php
 
-require('phpusage.class.php');
+// We will only do statistict for cli mode. It doesn't make much sense for
+// most web servers
+if (php_sapi_name() === 'cli') {
+	require('phpusage.class.php');
 
-// We use register_shutdown_function instead of auto_append_file because
-// auto_append_file will NOT execute appended code if the original script
-// calls exit()
-register_shutdown_function(array('PhpUsage', 'logUsage'));
+	// We use register_shutdown_function instead of auto_append_file because
+	// auto_append_file will NOT execute appended code if the original script
+	// calls exit()
+	register_shutdown_function(array('PhpUsage', 'logUsage'));
+}
 
 ?>
